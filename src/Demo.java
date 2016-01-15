edffc9b6143d58f039631d478f48c64fceff02f2
thumbimigwe <thumbimigwe@gmail.com>
2016-01-15 07:01:14 +0300

    created classes

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		soldier ghost = new soldier (8,1500);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/soldier.java
@@ -0,0 +1,17 @@
+
+public class soldier {
+	private int lives;
+	private int hitPoints;
+	private int remainigHitpoint;
+	
+	public soldier(int newLilives,int newHitpoints){
+		this.lives=newlives;
+		this.hitPoints=newhitpoints;
+	}
+	
+	
+	
+
+	
+
+}
