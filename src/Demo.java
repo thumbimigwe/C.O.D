1861fc57f0b8dadb618f421c8bce7636c2a057bc
thumbimigwe <thumbimigwe@gmail.com>
2016-01-15 07:31:58 +0300

    Demo class

--- src/Demo.java
+++ src/Demo.java
@@ -3,6 +3,14 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		soldier ghost = new soldier (8,1500);
+		ghost.takeDamage(150);
+		System.out.println("soldier");
+		System.out.println("Hit Point: "+ ghost.getremainingHitpoint()+"/" + ghost.getHitPoints());
+		System.out.println("Lives: "+ghost.getLives());
+		System.out.println();
+		
+		
+		
 		
 	
 
